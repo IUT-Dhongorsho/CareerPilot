988c60853e2b6b046281bf1c451dfa8003fcd297
ZoldyckX <tabibhassan.1604@gmail.com>
2026-06-08 00:01:04 +0600

    Random

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import RiveAnimation from './RiveAnimation'
 
 function App() {
   const [count, setCount] = useState(0)
